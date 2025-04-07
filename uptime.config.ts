caa21553793714d053bedd221f7857a82ceb4d2c
dododook <130355324+dododook@users.noreply.github.com>
2025-04-07 14:34:44 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,28 +1,92 @@
 const pageConfig = {
-  title: "ron's Status Page",
+  // Title for your status page
+  title: "lyc8503's Status Page",
+  // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
     { link: 'https://github.com/dododook', label: 'GitHub' },
-    { link: 'mailto:yaoguangting@gmail.com', label: 'Email Me', highlight: true },
+    { link: 'https://128128.best/', label: 'Blog' },
+    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
   ],
+  // [OPTIONAL] Group your monitors
+  // If not specified, all monitors will be shown in a single list
+  // If specified, monitors will be grouped and ordered, not-listed monitors will be invisble (but still monitored)
+  group: {
+    "🌐 Public (example group name)": ['foo_monitor', 'bar_monitor', 'more monitor ids...'],
+    "🔐 Private": ['test_tcp_monitor'],
+  },
 }
 
 const workerConfig = {
+  // Write KV at most every 3 minutes unless the status changed
   kvWriteCooldownMinutes: 3,
-  passwordProtection: 'admin:admin',
+  // Enable HTTP Basic auth for status page & API by uncommenting the line below, format `<USERNAME>:<PASSWORD>`
+  // passwordProtection: 'username:password',
+  // Define all your monitors here
   monitors: [
+    // Example HTTP Monitor
     {
-      id: 'google_monitor',
-      name: 'My Google Monitor',
+      // `id` should be unique, history will be kept if the `id` remains constant
+      id: 'foo_monitor',
+      // `name` is used at status page and callback message
+      name: 'My API Monitor',
+      // `method` should be a valid HTTP Method
       method: 'GET',
-      target: 'https://vps.128128.best'
+      // `target` is a valid URL
+      target: 'https://vps.128128.best',
+      // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
+      tooltip: 'This is a tooltip for this monitor',
+      // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
+      statusPageLink: 'https://vps.128128.best',
+      // [OPTIONAL] `hideLatencyChart` will hide status page latency chart if set to true
+      hideLatencyChart: false,
+      // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
+      expectedCodes: [200],
+      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
+      timeout: 10000,
+      // [OPTIONAL] headers to be sent
+      headers: {
+        'User-Agent': 'Uptimeflare',
+        Authorization: 'Bearer YOUR_TOKEN_HERE',
+      },
+      // [OPTIONAL] body to be sent
+      body: 'Hello, world!',
+      // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
+      responseKeyword: 'success',
+      // [OPTIONAL] if specified, the response must NOT contains the keyword to be considered as operational.
+      responseForbiddenKeyword: 'bad gateway',
+      // [OPTIONAL] if specified, the check will run in your specified region,
+      // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
+      checkLocationWorkerRoute: 'https://xxx.example.com',
     },
+    // Example TCP Monitor
     {
-      id: 'ssh_monitor',
-      name: 'Example SSH Monitor',
+      id: 'test_tcp_monitor',
+      name: 'Example TCP Monitor',
+      // `method` should be `TCP_PING` for tcp monitors
       method: 'TCP_PING',
-      target: '47.242.150.235:22'
+      // `target` should be `host:port` for tcp monitors
+      target: '47.242.150.235:22',
+      tooltip: 'My production server SSH',
+      statusPageLink: 'https://example.com',
+      timeout: 5000,
     },
   ],
+  notification: {
+    // [Optional] apprise API server URL
+    // if not specified, no notification will be sent
+    appriseApiServer: "https://apprise.example.com/notify",
+    // [Optional] recipient URL for apprise, refer to https://github.com/caronc/apprise
+    // if not specified, no notification will be sent
+    recipientUrl: "tgram://bottoken/ChatID",
+    // [Optional] timezone used in notification messages, default to "Etc/GMT"
+    timeZone: "Asia/Shanghai",
+    // [Optional] grace period in minutes before sending a notification
+    // notification will be sent only if the monitor is down for N continuous checks after the initial failure
+    // if not specified, notification will be sent immediately
+    gracePeriod: 5,
+    // [Optional] disable notification for monitors with specified ids
+    skipNotificationIds: ['foo_monitor', 'bar_monitor'],
+  },
   callbacks: {
     onStatusChange: async (
       env: any,
@@ -30,7 +94,7 @@ const workerConfig = {
       isUp: boolean,
       timeIncidentStart: number,
       timeNow: number,
-      reason: string,
+      reason: string
     ) => {
       // This callback will be called when there's a status change for any monitor
       // Write any Typescript code here
@@ -43,7 +107,7 @@ const workerConfig = {
       monitor: any,
       timeIncidentStart: number,
       timeNow: number,
-      reason: string,
+      reason: string
     ) => {
       // This callback will be called EVERY 1 MINTUE if there's an on-going incident for any monitor
       // Write any Typescript code here
